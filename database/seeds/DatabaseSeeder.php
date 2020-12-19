22e102d0f5cfe36c1fc71004e20c3c9ed87f2cd0
Victor Danilo <victordanilo_cs@live.com>
2020-12-19 12:27:52 -0300

    Add status de pedidos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(RolesAndPermissionsSeeder::class);
+        $this->call(ProductCategorySeeder::class);
+        $this->call(OrderStatusSeeder::class);
     }
 }
